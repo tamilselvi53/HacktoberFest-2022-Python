d42639cda7e1d0a9d24f5de9b2f4055c30a14aa5
anamaya <sharma.011999@gmail.com>
2022-09-30 23:56:11 +0530

    added binary_to_decimal test cases and readme.md

---
+++ easy/binary_to_decimal/README.md
@@ -0,0 +1,50 @@
+#Hacktoberfest 2022
+## Binary to Decimal Conversion in Python
+
+### Problem Statement
+Write a program to convert a binary number to decimal in Python.
+
+### Input Format
+Create a function with name binary_to_decimal that takes a binary number as an argument and returns the decimal equivalent of the binary number. If invalid input is entered, return -1.
+
+### Output Format
+Return an integer denoting the decimal equivalent of the binary number.
+
+### Sample Input
+```
+binary = 1010
+```
+
+### Sample Output
+```
+10
+```
+
+### Explanation
+The decimal equivalent of 1010 is 10.
+
+### Testing
+Run the following command:
+```
+python3 tests.py
+```
+
+#### Contributing
+If all test cases pass, your solution will be merged with the main branch. If you have any questions, feel free to open an issue.
+
+## Acknowledgments
+- [Python](https://www.python.org/)
+- [Hacktoberfest](https://hacktoberfest.digitalocean.com/)
+- [GitHub](https://github.com)
+- [Git](https://git-scm.com/)
+
+## Maintainer
+- [Anamaya](https://www.linkedin.com/in/anamaya1729/)
+- [Kartik](https://github.com/kartik007007)
+- [Shantanu](https://github.com/neutralWire)
+- [Shailesh](https://github.com/ShaileshKumar007)
+
+## License
+**This project is licensed under the GNU GENERAL PUBLIC License - see the [LICENSE](../../LICENSE) file for details**
+
+## Happy Coding! :smile:
---
+++ easy/binary_to_decimal/tests.py
@@ -0,0 +1,49 @@
+import unittest
+import utility
+from solution import solution
+
+class Test(unittest.TestCase):
+    """Binary to decimal Test"""
+    def test_binary_to_decimal_test_a(self):
+        """Normal Test 1"""
+        self.assertEqual(solution(1010), 10, "Test 1 Failed")
+
+    def test_binary_to_decimal_test_b(self):
+        """Normal Test 2"""
+        self.assertEqual(solution(1011), 11, "Test 2 Failed")
+
+    def test_binary_to_decimal_test_c(self):
+        """Normal Test 3"""
+        self.assertEqual(solution(1001), 9, "Test 3 Failed")
+
+    def test_binary_to_decimal_test_d(self):
+        """Normal Test 4"""
+        self.assertEqual(solution(10101001), 169, "Test 4 Failed")
+
+    def test_binary_to_decimal_test_e(self):
+        """Normal Test 5"""
+        self.assertEqual(solution(111111111111111111111111111111), 4294967295, "Test 5 Failed")
+
+    def test_binary_to_decimal_test_f(self):
+        """Normal Test 6"""
+        self.assertEqual(solution(1111111111111111111111111111111), 8589934591, "Test 6 Failed")
+
+    def test_binary_to_decimal_test_g(self):
+        "Exception Test 1"""
+        self.assertEqual(solution(), -1, "Test 7 Failed")
+
+    def test_binary_to_decimal_test_h(self):
+        """Exception Test 2"""
+        self.assertEqual(solution(123), -1, "Test 8 Failed")
+
+    def test_binary_to_decimal_test_i(self):
+        """Exception Test 3"""
+        self.assertEqual(solution(123456789), -1, "Test 9 Failed")
+
+    def test_binary_to_decimal_test_j(self):
+        """Exception Test 4"""
+        self.assertEqual(solution("1010"), 10, "Test 10 Failed")
+
+
+if __name__ == "__main__":
+    unittest.main()
