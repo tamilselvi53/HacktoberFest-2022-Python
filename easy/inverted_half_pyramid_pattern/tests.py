a1e0eb0eec638e824a2fbacc56de8e133b9fea67
Shantanu Rajput <90391285+neutralWire@users.noreply.github.com>
2022-10-04 10:22:18 +0530

    Updated pattern printing

--- easy/inverted_half_pyramid_pattern/tests.py
+++ easy/inverted_half_pyramid_pattern/tests.py
@@ -7,19 +7,19 @@ class Test(unittest.TestCase):
 
     def test_half_diamond_a(self):
         """Normal Test 1"""
-        self.assertEqual(solution(5), "*****\n****\n***\n**\n*", "Normal Test 1 Failed")
+        self.assertEqual(solution(5), "*********\n *******\n  *****\n   ***\n    *", "Normal Test 1 Failed")
 
     def test_half_diamond_b(self):
         """Normal Test 2"""
-        self.assertEqual(solution(3), "***\n**\n*", "Normal Test 2 Failed")
+        self.assertEqual(solution(3), "*****\n ***\n  *", "Normal Test 2 Failed")
 
     def test_half_diamond_c(self):
         """Normal Test 3"""
-        self.assertEqual(solution(7), "*******\n******\n*****\n****\n***\n**\n*", "Normal Test 3 Failed")
+        self.assertEqual(solution(7), "*************\n ***********\n  *********\n   *******\n    *****\n     ***\n      *", "Normal Test 3 Failed")
 
     def test_half_diamond_d(self):
         """Normal Test 4"""
-        self.assertEqual(solution(9), "*********\n********\n*******\n******\n*****\n****\n***\n**\n*", "Normal Test 4 Failed")
+        self.assertEqual(solution(9), "*****************\n ***************\n  *************\n   ***********\n    *********\n     *******\n      *****\n       ***\n        *", "Normal Test 4 Failed")
 
     def test_half_diamond_e(self):
         """Exception Test 1"""
