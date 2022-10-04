13237aedbc70c8401b22cf96d81a4dbbda90ad30
Shantanu Rajput <90391285+neutralWire@users.noreply.github.com>
2022-10-04 10:37:25 +0530

    initial changes

---
+++ easy/inverted_right_angle_triangle_pattern/tests.py
@@ -0,0 +1,37 @@
+import unittest
+import utility
+from solution import solution
+
+class Test(unittest.TestCase):
+    """Half Diamond Test Cases"""
+
+    def test_half_diamond_a(self):
+        """Normal Test 1"""
+        self.assertEqual(solution(5), "*****\n****\n***\n**\n*", "Normal Test 1 Failed")
+
+    def test_half_diamond_b(self):
+        """Normal Test 2"""
+        self.assertEqual(solution(3), "***\n**\n*", "Normal Test 2 Failed")
+
+    def test_half_diamond_c(self):
+        """Normal Test 3"""
+        self.assertEqual(solution(7), "*******\n******\n*****\n****\n***\n**\*", "Normal Test 3 Failed")
+
+    def test_half_diamond_d(self):
+        """Normal Test 4"""
+        self.assertEqual(solution(9), "*********\n********\n*******\n******\n*****\n****\n***\n**\n*", "Normal Test 4 Failed")
+
+    def test_half_diamond_e(self):
+        """Exception Test 1"""
+        self.assertEqual(solution(0), -1, "Exception Test 1 Failed")
+
+    def test_half_diamond_f(self):
+        """Exception Test 2"""
+        self.assertEqual(solution(-1), -1, "Exception Test 2 Failed")
+
+    def test_half_diamond_g(self):
+        """Exception Test 3"""
+        self.assertEqual(solution(), -1, "Exception Test 3 Failed")
+
+if __name__ == '__main__':
+    utility.main()
