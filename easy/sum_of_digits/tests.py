e563d2db3e637000e0fd8b2bc8f4a0ebc4c3c14a
Shantanu Rajput <90391285+neutralWire@users.noreply.github.com>
2022-10-04 13:07:40 +0530

    initial changes

---
+++ easy/sum_of_digits/tests.py
@@ -0,0 +1,40 @@
+import unittest
+import utility
+from solution import solution
+
+class Test(unittest.TestCase):
+    """Calculator Tests"""
+    def test_calculator_test_a(self):
+        """Normal Test 1"""
+        self.assertEqual(solution(123), 6, "Test 1 Failed")
+
+    def test_calculator_test_b(self):
+        """Normal Test 2"""
+        self.assertEqual(solution(2358), 18, "Test 2 Failed")
+
+    def test_calculator_test_c(self):
+        """Normal Test 3"""
+        self.assertEqual(solution(66666), 30, "Test 3 Failed")
+
+    def test_calculator_test_d(self):
+        """Normal Test 4"""
+        self.assertEqual(solution(1), 1, "Test 4 Failed")
+
+    def test_calculator_test_e(self):
+        """Exception Test 1"""
+        self.assertEqual(solution(0), 0, "Test 5 Failed")
+
+    def test_calculator_test_f(self):
+        """Exception Test 2"""
+        self.assertEqual(solution("a"), -1, "Test 6 Failed")
+
+    def test_calculator_test_g(self):
+        """Exception Test 3"""
+        self.assertEqual(solution(-1), -1, "Test 7 Failed")
+
+    def test_calculator_test_h(self):
+        """Exception Test 4"""
+        self.assertEqual(solution(-123), -1, "Test 8 Failed")
+
+if __name__ == "__main__":
+    unittest.main()
