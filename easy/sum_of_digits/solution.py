4ea5b95727d8865a74ffa31133cf7a096f4aa026
Shubhang <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-14 01:50:54 +0530

    Update solution.py
    Created function sum of digits

---
+++ easy/sum_of_digits/solution.py
@@ -0,0 +1,16 @@
+def solution(n):
+    if  not str(n).isnumeric():
+        return -1
+    
+    if int(n) == 0:
+        return 0
+        
+    if int(n)<0:
+        return -1
+
+    sum = 0
+    for digit in str(n): 
+      sum += int(digit)      
+    return sum
+   
+
