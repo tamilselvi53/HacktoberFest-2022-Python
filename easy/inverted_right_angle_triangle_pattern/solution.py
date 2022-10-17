490903d4a3d721c2fdea74d8d270b19f3aa172a4
Shantanu Rajput <shantanurajput520@gmail.com>
2022-10-17 12:39:19 +0530

    Solution for Inverted Right Angle Triangle Problem

---
+++ easy/inverted_right_angle_triangle_pattern/solution.py
@@ -0,0 +1,14 @@
+def solution(n=-1):
+    if n==-1:
+        return -1
+    if n<1:
+        return -1
+    string= ""
+    if(n>0):
+        for row in range(n):
+            for column in range(n-row):
+                string+="*"
+            if row==n-1:
+                break
+            string+="\n"
+    return string
