fe108d44eea54f4f92068e839baf3c47cc671ede
Tamilselvi <85762097+tamilselvi53@users.noreply.github.com>
2022-10-22 23:24:43 +0530

    insertion sort algorithm added

--- easy/insertion_sort/solution.py
+++ easy/insertion_sort/solution.py
@@ -1,2 +1,11 @@
 def solution():
-    pass
+    for i in range(1, len(List)):
+        currentNumber = List[i]
+        for j in range(i - 1, -1, -1):
+            if List[j] > currentNumber :
+                List[j], List[j + 1] = List[j + 1], List[j]
+            else:
+                List[j + 1] = currentNumber
+                break
+
+    return List
